09b8a10fa7764399820f40910193c873d5c44a47
ashifanassar <ashifanassar@gmail.com>
2024-04-29 12:30:10 +0530

    Modules

---
+++ elastiCache.tf
@@ -0,0 +1,24 @@
+# Provisions Elastic-Cache Cluster 
+resource "aws_elasticache_cluster" "redis" {
+  cluster_id           = "roboshop-${var.ENV}-redis"
+  engine               = "redis"
+  node_type            = "cache.t3.small"
+  num_cache_nodes      = 1
+  parameter_group_name = aws_elasticache_parameter_group.redis_pg.name
+  engine_version       = "6.x"
+  port                 = 6379
+  subnet_group_name    = aws_elasticache_subnet_group.redis.name 
+  security_group_ids   = [aws_security_group.allow_redis.id]
+}
+
+# Elastic-Cache Parameter Group 
+resource "aws_elasticache_parameter_group" "redis_pg" {
+  name                 = "roboshop-${var.ENV}-redis-pq"
+  family               = "redis6.x"
+}
+
+# Creates a Subnet Group For the Groups-Of-Subnets 
+resource "aws_elasticache_subnet_group" "redis" {
+  name                 = "roboshop-${var.ENV}-redis-subnetgroup"
+  subnet_ids           = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+}
---
+++ sg.tf
@@ -0,0 +1,25 @@
+# security groups that allows only ws netwokr and internal network
+resource "aws_security_group" "allow_redis" {
+  name                    = "roboshop-${var.ENV}-redis-sg"
+  description             = "roboshop-${var.ENV}-redis-sg"
+  vpc_id                  = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+  ingress {
+    from_port       = 6379
+    to_port         = 6379
+    protocol        = "tcp"
+    cidr_blocks     = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.ENV}-redis-sg"
+  }
+}
